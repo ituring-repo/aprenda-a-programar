69e8916013e5471055c7e75cd04b8137dae419db
Tiago Sestari <tiagosestari@gmail.com>
2022-04-24 11:35:00 -0300

    Adicionando elementos da aula 3

---
+++ Aula 3/Gabaritos/Readme.md
@@ -0,0 +1,14 @@
+# Gabarito exercício Lógica
+
+```
+Exercício 1 é A
+Exercício 2 é B
+Exercício 3 é A
+Exercício 4 é A
+Exercício 5 é A
+Exercício 6 é A
+Exercício 7 é A
+Exercício 8 é A
+Exercício 9 é B
+Exercício 10 é A
+```
---
+++ Aula 3/Projeto para casa/logica.js
@@ -0,0 +1,69 @@
+//Análise os pedaços de código a seguir e tente prever qual será o console.log resultante, A ou B.
+//Você pode rodar os código para verificar suas respostas.
+
+let a = 10
+let b = true
+let c = []
+let d = [1,2,3,4]
+let e = "Nome"
+
+//1
+if (a == 10) {
+    console.log("A");
+} else {
+    console.log("B");
+}
+//2
+if (a > 10) {
+    console.log("A");
+} else {
+    console.log("B");
+}
+//3
+if (a >= 10) {
+    console.log("A");
+} else {
+    console.log("B");
+}
+//4
+if (b == true) {
+    console.log("A");
+} else {
+    console.log("B");
+}
+//5
+if (b) {
+    console.log("A");
+} else {
+    console.log("B");
+}
+//6
+if (c) {
+    console.log("A");
+} else {
+    console.log("B");
+}
+//7
+if (d) {
+    console.log("A");
+} else {
+    console.log("B");
+}
+//8
+if (d[0] == 1) {
+    console.log("A");
+} else {
+    console.log("B");
+}
+//9
+if (d[1] == 1) {
+    console.log("A");
+} else {
+    console.log("B");
+}
+//10
+if (typeof(e) == 'string') {
+    console.log("A");
+} else {
+    console.log("B");
+}
