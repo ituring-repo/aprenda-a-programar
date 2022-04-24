3f11809bf2e1c45793165177de60c07a83f13571
Tiago Sestari <tiagosestari@gmail.com>
2022-04-24 11:07:13 -0300

    Exercícios aulas 3 e 4

---
+++ Aula 4/Codigo usado em aula/alunos.js
@@ -0,0 +1,29 @@
+//Listas [ ]
+//Mapas {chave: valor, chave: valor}
+//Lista de mapas: [{chave: valor, chave: valor}, {chave: valor, chave: valor}]
+
+let arr = [
+    {'nome': 'Tiago', 'fezProva': true, 'media': 10},
+    {'nome': 'Alvaro', 'fezProva': true, 'media': 9},
+    {'nome': 'Tobias', 'fezProva': false, 'media': 8},
+    {'nome': 'Regina', 'fezProva': true, 'media': 9},
+    {'nome': 'Julia', 'fezProva': true, 'media': 5},
+    {'nome': 'Joana', 'fezProva': false, 'media': 6},
+    {'nome': 'Paulo', 'fezProva': false, 'media': 4},
+    {'nome': 'Carla', 'fezProva': false, 'media': 2},
+    {'nome': 'Luciana', 'fezProva': true, 'media': 1}
+]
+
+//vamos analisar alguns logs para entender nossos dados
+//console.log(arr[0])
+//console.log(arr[1])
+
+//console.log(arr[0].nome)
+//console.log(arr[1].nome)
+
+
+//Exercício 1: passando pela lista imprima apenas os nomes de alunos que fizeram a prova.
+//Exercício 2: quantos alunos fizeram a prova?
+//Exercício 3: passando pela lista, imprima apenas os nomes de alunso que fizeram a prova e tiveram média acima de 5.
+
+
