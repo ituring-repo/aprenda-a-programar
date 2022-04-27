d4325e561fbcf922f8e12eed99a79ede46cde60b
Tiago Sestari <tiagosestari@gmail.com>
2022-04-27 20:54:10 -0300

    Rascunho do código da aula. Pendente formatação final

--- Aula 3/Codigo usado em aula/alunos.js
+++ Aula 3/Codigo usado em aula/alunos.js
@@ -1,3 +1,72 @@
+
+
+console.log("Olá, Mundo! Sou o primeiro programa da ITuring.")
+
+let idade = 18 //idade recebe o valor 18
+let nome = 'Tiago Sestari'
+let boolA = true
+let boolB = false
+let lista = [18, 20, 40, 'Thales', 10, 'Tiago', [10, 20, 30, 'david']]
+
+let mapa = {
+    "faculdade": ['boletim', 'carterinha', 'provas'],
+    "casamento": ['casa', 'certidão'],
+    "anos_de_casado": 30
+}
+
+// console.log(idade)
+// console.log(nome)
+// console.log(boolA)
+// console.log(boolB)
+// console.log(lista)
+// console.log(mapa)
+//console.log(lista[5])
+//console.log(mapa['anos_de_casado'])
+
+//Tipagem Estática: eu preciso dizer para o computador o tipo de variável que estou criando.
+//Tipagem Dinâmica: o computador já percebe o tipo de variável que está sendo usada.
+
+// let numero = 10
+// console.log(numero)
+// numero = "Teste de texto"
+// console.log(numero)
+
+let numero_a = 10
+let numero_b = 15
+let soma = (numero_a + numero_b) / 2
+//console.log(soma)
+
+nome = "Tiago"
+let sobrenome = "Sestari"
+//console.log(nome + sobrenome)
+
+
+//DESAFIO
+let texto = "11"
+let numero = 1
+//console.log(texto + numero)
+//console.log(texto - numero)
+
+
+
+//CONDICIONAIS
+if (numero != 2) {
+    console.log("É Verdade")
+} else {
+    console.log("Não é verdade")
+}
+
+/*
+
+< - é menor
+> - é maior
+== - é igual
+!= - é diferente
+>= - é maior ou igual
+<= - é menor ou igual
+
+*/
+
 //Listas [ ]
 //Mapas {chave: valor, chave: valor}
 //Lista de mapas: [{chave: valor, chave: valor}, {chave: valor, chave: valor}]
