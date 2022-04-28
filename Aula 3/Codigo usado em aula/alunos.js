c6c72b75e919625976c5d2061b4c05776c0be5a9
Tiago Sestari <tiagosestari@gmail.com>
2022-04-28 11:05:20 -0300

    Versão editada do código de aula

--- Aula 3/Codigo usado em aula/alunos.js
+++ Aula 3/Codigo usado em aula/alunos.js
@@ -1,40 +1,48 @@
 
-
+//Usando o Console.Log
 console.log("Olá, Mundo! Sou o primeiro programa da ITuring.")
 
+
+//Declarando variáveis
 let idade = 18 //idade recebe o valor 18
 let nome = 'Tiago Sestari'
 let boolA = true
 let boolB = false
 let lista = [18, 20, 40, 'Thales', 10, 'Tiago', [10, 20, 30, 'david']]
-
 let mapa = {
     "faculdade": ['boletim', 'carterinha', 'provas'],
     "casamento": ['casa', 'certidão'],
     "anos_de_casado": 30
 }
 
-// console.log(idade)
-// console.log(nome)
-// console.log(boolA)
-// console.log(boolB)
-// console.log(lista)
-// console.log(mapa)
-//console.log(lista[5])
-//console.log(mapa['anos_de_casado'])
+//Vendo as variáveis na tela
+console.log(idade)
+console.log(nome)
+console.log(boolA)
+console.log(boolB)
+console.log(lista)
+console.log(mapa)
+
+//Podemos acessar pedaços de listas e mapas
+console.log(lista[5])
+console.log(mapa['anos_de_casado'])
 
 //Tipagem Estática: eu preciso dizer para o computador o tipo de variável que estou criando.
 //Tipagem Dinâmica: o computador já percebe o tipo de variável que está sendo usada.
 
-// let numero = 10
-// console.log(numero)
-// numero = "Teste de texto"
-// console.log(numero)
 
+//Podemos trocar o valor de uma variável sem precisar usar o let para declarar novamente.
+let numero = 10
+console.log(numero)
+numero = "Teste de texto"
+console.log(numero)
+
+
+//Podemos fazer contas com variáveis
 let numero_a = 10
 let numero_b = 15
 let soma = (numero_a + numero_b) / 2
-//console.log(soma)
+console.log(soma)
 
 nome = "Tiago"
 let sobrenome = "Sestari"
@@ -66,26 +74,3 @@ if (numero != 2) {
 <= - é menor ou igual
 
 */
-
-//Listas [ ]
-//Mapas {chave: valor, chave: valor}
-//Lista de mapas: [{chave: valor, chave: valor}, {chave: valor, chave: valor}]
-
-let arr = [
-    {'nome': 'Tiago', 'fezProva': true, 'media': 10},
-    {'nome': 'Alvaro', 'fezProva': true, 'media': 9},
-    {'nome': 'Tobias', 'fezProva': false, 'media': 8},
-    {'nome': 'Regina', 'fezProva': true, 'media': 9},
-    {'nome': 'Julia', 'fezProva': true, 'media': 5},
-    {'nome': 'Joana', 'fezProva': false, 'media': 6},
-    {'nome': 'Paulo', 'fezProva': false, 'media': 4},
-    {'nome': 'Carla', 'fezProva': false, 'media': 2},
-    {'nome': 'Luciana', 'fezProva': true, 'media': 1}
-]
-
-//vamos analisar alguns logs para entender nossos dados
-//console.log(arr[0])
-//console.log(arr[1])
-
-//console.log(arr[0].nome)
-//console.log(arr[1].nome)
