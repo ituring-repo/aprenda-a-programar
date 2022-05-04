14537958ce9e0a2b060d4d48e3ecd27f80df9918
Thales Moreira Carvalho <thales.comp@gmail.com>
2022-05-04 09:34:53 -0300

    Update script.js

--- Aula 5/Projeto para casa/Exercicio IMC/script.js
+++ Aula 5/Projeto para casa/Exercicio IMC/script.js
@@ -1,28 +1,27 @@
 
-const IMC = (peso, altura) => {
+function calculoIMC(peso, altura) {
     //insira sua lógica aqui do ex. 1 aqui.
     
 
 }
 
-const faixaIMC = (indice) => {
+function resultadoIMC(indiceIMC) {
     //insira sua lógica do ex.2 aqui.
     //dica, lembre-se que quando queremos duas condições simultaneas, usamos o &&
     //conforme vimos em aula, caso a pessoa precise estar 
     //acima de um imc E abaixo de outro, podemos fazer algo como 
-    // if (x < 10 && x > 5)
+    // if (x > 5 && x < 10)
 
-    //não esqueça de retornar o texto da faixa.
-
+    // não esqueça de retornar o texto da faixa.
 }
 
-const pegarValores = () => {
-    let num1 = Number(document.getElementById("peso").value)
-    let num2 = Number(document.getElementById("altura").value)
+function mostrarResultadoIMC() {
+    let peso = Number(document.getElementById("peso").value)
+    let altura = Number(document.getElementById("altura").value)
 
-    let imc = IMC(num1, num2)
+    let imc = calculoIMC(peso, altura)
     document.getElementById("resultado").innerText = "O seu IMC é: " + imc
 
-    let faixa = faixaIMC(imc)
-    alert("Sua faixa de IMC é: " + faixa);
+    let faixaIMC = resultadoIMC(imc)
+    alert("Sua faixa de IMC é: " + faixaIMC);
 }
