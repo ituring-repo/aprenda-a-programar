e3373c32f20a9de2a48a7d7d773ef37e53801e18
Thales Carvalho <thales.comp@gmail.com>
2022-08-22 16:00:52 -0300

    ajuste das tarefas

--- tarefa-dia-2/solucao/solucao-pratica-1.js
+++
@@ -1,9 +0,0 @@
-let nome = "Maria"
-let email = "maria@maria.com"
-let profissao = "publicitária"
-let idade = 33
-let viagemDosSonhos = "Bali"
-
-let mensagem = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + viagemDosSonhos + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
-
-console.log(mensagem)
--- tarefa-dia-3/solucao/solucao-pratica-2.js
+++
@@ -1,4 +0,0 @@
-for (let contador = 1; contador <= 10; contador++) {
-    let resultado = contador*7
-    console.log("7 x", contador, "=", resultado)
-}
